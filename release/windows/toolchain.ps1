f793389a3da33069dd33f5bfb8cf6dbed14873db
Mark D. Adams <deadluser@gmail.com>
2025-09-18 18:36:08 -0700

    Update toolchain.ps1

--- release/windows/toolchain.ps1
+++ release/windows/toolchain.ps1
@@ -12,9 +12,9 @@ $global:LinkerFlags = @(
     '/PDBALTPATH:%_PDB%'
 )
 
-$global:VsInstallPrefix = Join-Path ${env:ProgramFiles} 'Microsoft Visual Studio\2022' 'Community'
-$global:VswherePath = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio\Installer' 'vswhere.exe'
-$global:VcVarsScript = Join-Path $VsInstallPrefix 'VC\Auxiliary\Build' 'vcvarsall.bat'
+$global:VsInstallPrefix = Join-Path ${env:ProgramFiles} 'Microsoft Visual Studio\2022\Community'
+$global:VswherePath = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio\Installer\vswhere.exe'
+$global:VcVarsScript = Join-Path $VsInstallPrefix 'VC\Auxiliary\Build\vcvarsall.bat'
 
 # Use vswhere to get the semantic version (strip anything after '+')
 $global:VsVersion = (& $VswherePath -Property catalog_productSemanticVersion -Path $VsInstallPrefix) -split '\+' | Select-Object -First 1
