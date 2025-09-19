b9d6937342f8d71d80c055918c3dc341e84d356d
Mark D. Adams <deadluser@gmail.com>
2025-09-19 08:13:44 -0700

    Fix path format for remove_directory command

--- release/windows/build-curl.ps1
+++ release/windows/build-curl.ps1
@@ -41,6 +41,6 @@ function global:Build-Curl([string] $PrefixDir, [string] $Arch, [string] $DepsPr
     )
 
     Invoke-CMakeBuildAndInstall $SourceDir $BuildDir $ConfigOptions
-    Invoke-NativeCommand cmake -E remove_directory (Join-Path $PrefixDir lib cmake CURL) # until we support it
+    Invoke-NativeCommand cmake -E remove_directory (Join-Path $PrefixDir 'lib\cmake\CURL') # until we support it
     Copy-Item -Path (Join-Path $BuildDir lib libcurl.pdb) -Destination (Join-Path $PrefixDir bin)
 }
